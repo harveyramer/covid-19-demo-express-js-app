2d896d7e3e0cf20cb1d3aa11c0f2ece1f32f2460
Kendrick <Kendubu1@users.noreply.github.com>
2022-01-21 15:17:52 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const routes = require('./routes');
 const app = express();
 require('dotenv').config();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
